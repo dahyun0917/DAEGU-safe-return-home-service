73b04445d7f689defa1015fd01217f7c2436549e
dahyun <dahyun0917@naver.com>
2021-11-25 16:15:22 +0900

    메세지 기능_ 멀티 권한 수정 필요

--- app/src/main/java/com/example/safe_return_home_service/signal.kt
+++ app/src/main/java/com/example/safe_return_home_service/signal.kt
@@ -15,6 +15,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.IOException
 import java.util.*
+import java.util.jar.Manifest
 
 
 class signal: AppCompatActivity() {
@@ -32,13 +33,67 @@ class signal: AppCompatActivity() {
 
     lateinit var sms : SmsManager
 
+    private val multiplePermissionsCode = 100
+    private val requiredPermissions = arrayOf(
+        android.Manifest.permission.SEND_SMS,
+        android.Manifest.permission.READ_SMS,
+        android.Manifest.permission.RECORD_AUDIO,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signal)
         btn_cancle = findViewById(R.id.btn_cancle)
+        /*val permissions=ArrayList<String>()
+        if ((ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_SMS) +
+                    ContextCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS))
+            != PackageManager.PERMISSION_GRANTED) {
 
-        //SendSMS();
-        if (ContextCompat.checkSelfPermission(this,
+            permissions.add(android.Manifest.permission.READ_SMS)
+            permissions.add(android.Manifest.permission.SEND_SMS)
+            /*val permissions = arrayOf(android.Manifest.permission.READ_SMS,
+                android.Manifest.permission.SEND_SMS)
+            ActivityCompat.requestPermissions(this, permissions,0)*/
+        }
+        else if(ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(android.Manifest.permission.RECORD_AUDIO)
+            permissions.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            permissions.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            //Permission is not granted
+            /*val permissionss = arrayOf(android.Manifest.permission.RECORD_AUDIO,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissionss,0)*/
+        }
+        if(permissions.isNotEmpty()){
+            val array = arrayOfNulls<String>(permissions.size)
+            ActivityCompat.requestPermissions(this, permissions.toArray(array), multiplePermissionsCode)
+        }*/
+
+        var rejectedPermissionList = ArrayList<String>()
+        for(permission in requiredPermissions){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                //만약 권한이 없다면 rejectedPermissionList에 추가
+                rejectedPermissionList.add(permission)
+            }
+        }
+        if(rejectedPermissionList.isNotEmpty()){
+            //권한 요청!
+            val array = arrayOfNulls<String>(rejectedPermissionList.size)
+            ActivityCompat.requestPermissions(this, rejectedPermissionList.toArray(array), multiplePermissionsCode)
+            SendSMS()
+            startRecording()
+        }
+        else{
+            SendSMS()
+            startRecording()
+        }
+
+
+
+        /*if (ContextCompat.checkSelfPermission(this,
                 android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             //Permission is not granted
@@ -48,7 +103,7 @@ class signal: AppCompatActivity() {
             startRecording()
         } else {
             startRecording()
-        }
+        }*/
         btn_cancle.setOnClickListener {
             timerTask?.cancel();
             count=1
@@ -117,16 +172,16 @@ class signal: AppCompatActivity() {
             }
         }
     }
-    /*fun SendSMS(){
-        var phoneNo = "01025335441";
+    fun SendSMS(){
+        //var phoneNo = "01025335441";
         //var sms = "안녕";
 
         sms = SmsManager.getDefault()
-        sms.sendTextMessage(phoneNo, null, "안녕", null,null)
+        sms.sendTextMessage("01025335441", null, "안녕", null,null)
+        Toast.makeText(this@signal, "문자발송.", Toast.LENGTH_SHORT).show()
 
 
-
-    }*/
+    }
 
 
 
